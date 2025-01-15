9630917c1d6fa1fde3cfa3a6932e2fb69a4e6bb4
LeoGuimas <gleo.dev0@gmail.com>
2025-01-15 15:55:29 -0300

    add layout and components for dashboard and user profiles; include basic structure for About and Analytics pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 
+import Hello from './components/hello';
+
 export default function Home() {
   console.log("What am i? -- SERVER/CLIENT?")
   return (
+    <>
     <h1 className="text-3xl">Welcome to Next.js</h1>
+    <Hello/>
+    </>
     );
 }
